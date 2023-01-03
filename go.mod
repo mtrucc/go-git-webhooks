1b21ef62ec2dfb083b6496f216b3a0c0b051e3ac
mtrucc <mtrucc@qq.com>
2023-01-03 18:02:06 +0800

    ip查询

--- go.mod
+++ go.mod
@@ -12,7 +12,9 @@ require (
 	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/lionsoul2014/ip2region/binding/golang v0.0.0-20221209023550-d117f72d584d // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/lionsoul2014/ip2region/binding/golang/xdb"
 	"log"
 	"net/http"
 )
@@ -10,6 +11,31 @@ import (
 func main() {
 	fmt.Println("测试")
 	router := gin.Default()
+
+	var dbPath = "ip2region.xdb"
+	searcher, err := xdb.NewWithFileOnly(dbPath)
+	if err != nil {
+		fmt.Printf("failed to create searcher: %s\n", err.Error())
+		return
+	}
+
+	defer searcher.Close()
+
+	// 获取 IP
+	router.GET("/ip", func(c *gin.Context) {
+		ip := c.ClientIP()
+		c.String(http.StatusOK, ip)
+	})
+
+	// 获取 IP ip2region 数据
+	router.GET("/ip2region", func(c *gin.Context) {
+		ip := c.ClientIP()
+		data, err := searcher.SearchByStr(ip)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, data)
+	})
 
 	router.POST("/post", func(c *gin.Context) {
 
@@ -25,7 +51,7 @@ func main() {
 		})
 
 	})
-	err := router.Run(":8080")
+	err = router.Run(":8080")
 	if err != nil {
 		fmt.Println("err:", err)
 	}
